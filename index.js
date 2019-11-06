f67db0b9ea11ae0d48aa2c5ec3b3c51ad20213c0
Walter Staeblein <walter@synergys.com.br>
2019-11-05 23:47:22 -0300

    Added update command

--- index.js
+++ index.js
@@ -34,6 +34,35 @@ module.exports = {
 		});
 	},
 
+	update: function(packages, opts){
+		if(packages.length == 0 || !packages || !packages.length){return Promise.reject("No packages found");}
+		if(typeof packages == "string") packages = [packages];
+		if(!opts) opts = {};
+		var cmdString = "npm update " + packages.join(" ") + " "
+		+ (opts.global ? " -g" : "")
+		+ (opts.save === false ? " --no-save" : " --save")
+		+ (opts.saveDev ? " --save-dev" : "");
+
+		return new Promise(function(resolve, reject){
+			var cmd = exec(cmdString, { cwd: opts.cwd ? opts.cwd : "/", maxBuffer: opts.maxBuffer ? opts.maxBuffer : 200 * 1024 }, (error, stdout, stderr) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(true);
+				}
+			});
+
+			if(opts.output) {
+				var consoleOutput = function(msg) {
+					console.log('npm: ' + msg);
+				};
+
+				cmd.stdout.on('data', consoleOutput);
+				cmd.stderr.on('data', consoleOutput);
+			}
+		});
+	},
+
 	uninstall: function(packages, opts){
 		if(packages.length == 0 || !packages || !packages.length){return Promise.reject(new Error("No packages found"));}
 		if(typeof packages == "string") packages = [packages];
